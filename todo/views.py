0ff015899dbddba635da25e3f9e849a929c21c57
Sanket Shete <140483948+sanketpshete@users.noreply.github.com>
2025-05-18 16:44:26 +0530

    mark as done function

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from todo.models import Task
 
 # Create your views here.
@@ -9,3 +9,10 @@ def addTask(request):
     Task.objects.create(task = new_task)
 
     return redirect('home')
+
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+
+    task.is_completed = True
+    task.save()
+    return redirect("home")
